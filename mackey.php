6940407d4ae50b629b610efca6d81eae65a1180d
chromatos <chromaesthesia@gmail.com>
2014-05-17 04:36:55 -0700

    Create mackey.php

---
+++ mackey.php
@@ -0,0 +1,144 @@
+<?php
+# An exec script to rejoin as Mr. Mackey, M'kay?
+
+#### jacking crutchy's exec script template:
+# gpl2
+# by crutchy
+# 17-may-2014
+
+//ini_set("display_errors","on"); # Too talkative; shows my php incompetence :-D
+
+require_once("lib.php");
+
+# alias|timeout|repeat|auto-privmsg|empty-trailing-allowed|php scripts/template.php %%trailing%% %%dest%% %%nick%% %%start%% %%alias%% %%cmd%% %%data%% %%exec%% %%params%%
+$trailing=$argv[1];
+$dest=$argv[2];
+$nick=$argv[3];
+$start=$argv[4];
+$alias=$argv[5];
+$cmd=$argv[6];
+$data=$argv[7];
+$exec=$argv[8];
+$params=$argv[9];
+####
+
+//$trailing = $argv[1]; # For local testing, disable above block and use this, M'kay?
+
+function isProper($message)
+{   if (($message[0] >= "A") && ($message[0] <= "Z")
+      ||($message[2] >= "A") && ($message[2] <= "Z")
+      || $message[1] == "'")
+        return(true);
+    else
+        return(false);
+}
+
+function isVowel($char)
+{   $z = "AEIOUaeiou";        # I'm just assuming it's slightly faster to do this
+    if (strpos($z, $char[0])) # than to use stripos but I'm not going to test it.
+        return(true);
+    else
+        return(false);
+}
+
+function isPlural($message)
+# This obviously handles every possible plural:
+
+{  // for ($z = 0; $z < strLen($message); $z++)
+    $l = strLen($message) - 1;
+
+    $has_s = $message[$l] == "s";
+    if ($message[$l-0] == "s" && $message[$l-1] == "s")
+        return(false);
+
+    if (isProper($message))
+        if ($message[$l] == "'")
+            return(true);
+        else
+            return(false);
+
+    if ($message[$l] == "s" && $message[$l-1] != "'")
+        return(true);
+
+#   I couldn't think of any words ending with "a" that weren't proper nouns or
+#   Latinate plurals so I put this in. It's not wrong; shut up!
+    if ((substr($message, $l-1, $l) == "ii") || $message[$l] == "a")
+        return(true);
+
+        return(false);
+}
+
+function pluralize($message)
+{   if (!isPlural($message))
+    {   $l = strlen($message) - 1;
+        if (substr($message, $l-1, $l) == "um")
+            return(substr($message, 0, $l-1)."a");
+        if (substr($message, $l, $l) == "y")
+            return(substr($message, 1, $l-1)."ies");
+        return($message."s");
+    }
+    else
+    return($message);
+}
+
+function prefix()
+{   switch (rand(0,8))
+    {   case 0: return("hmm, ");
+        case 1: return("well, ");
+        case 2: return("you see, ");
+        case 4: return("you see, kids, ");
+        case 5: return("now ");
+        case 6: return("uh ");
+    }
+}
+
+function singularInfix()
+{   switch (rand(0,3))
+    { case 0: return("a ");
+      case 1: return("the ");
+    }
+}
+
+function postfix()
+{   switch (rand(0,6))
+    {   case 0: return(".");
+        case 1: return("?");
+        case 2: return("!");
+    }
+}
+
+function sentence()
+{   global $verb, $nick, $trailing;
+    switch(rand(0, 6))
+    { case 0 : return("$nick, quit fooling around and get back to your work, m'kay");
+      default: return("$trailing$verb bad, m'kay");
+    }
+}
+
+$low         = strtolower($trailing);
+$nick        = "2b";
+$theSentence = "";
+
+if (strpos($low,"mkay") === false && strpos($low, "m'kay") === false)
+{   $theSentence .= prefix();
+
+    if (rand(0,2) == 0)      # We'll change it sometimes for fun.
+        pluralize($trailing);
+    $x = isPlural($trailing);
+
+    if ($x)
+    {   $verb = " are";
+    }
+    else
+    {   $verb = " is";
+        $theSentence .= singularInfix();
+    }
+    if (rand(0,1))
+        $theSentence[0] = strtoupper($theSentence[0]);
+    print $theSentence.sentence().postfix();
+}
+else
+    print "m'kay";
+
+print "\n";
+?>

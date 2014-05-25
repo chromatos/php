a07a0bcab7ad752056d7205919973f8346b0b949
chromatos <chromaesthesia@gmail.com>
2014-05-25 03:45:13 -0700

    Update langtoys.php
    Fancified it a bit but still missing some stuff because I got sidetracked by Misfits.

--- langtoys.php
+++ langtoys.php
@@ -2,21 +2,23 @@
 # Hackish language toys
 # License: WTFPL (See /copying)
 
-$keywords = array("beet", "radish", "monk", "robot", "cigarette");
+# Add whatever keywords you want to be picked up on:
+
+$keywords = array("crap", "poop", "beet", "radish", "monk", "robot", "cigarette", "donkey");
 
 function isProperNoun($message)
-{   return (  strlen($message) > 1 && (($message[0] >= "A") && ($message[0] <= "Z")
+{   return    strlen($message) > 1 && (($message[0] >= "A") && ($message[0] <= "Z")
            || strlen($message) > 2 && (($message[2] >= "A") && ($message[2] <= "Z"))
            || $message[1] == "'")
-           );
+           ;
 }
 
 function isVowel($char)
-{   $z = "AEIOUaeiou"; # I'm just assuming it's slightly faster to do this
+{   $z = "eaoiuEAOIU";        # I'm just assuming it's slightly faster to do this
     if (strpos($z, $char[0])) # than to use stripos but I'm not going to test it.
-        return(true);
+        return true;
     else
-        return(false);
+        return false;
 }
 
 function isPlural($message)
@@ -24,37 +26,46 @@ function isPlural($message)
 {   $l = strLen($message) - 1;
 
 #   Cheat for short words:
-    if ($l < 4) return($message[$l] == "s");
+    if ($l < 4) return $message[$l] == "s";
 
     $has_s = $message[$l] == "s";
     if ($message[$l-0] == "s" && $message[$l-1] == "s")
-        return(false);
+        return false;
 
     if (isProperNoun($message))
-        return($message[$l] == "'");
+        return $message[$l] == "'";
 
     if ($message[$l] == "s" && $message[$l-1] != "'")
-        return(true);
+        return true;
 
 # I couldn't think of many words ending with "a" that weren't proper nouns or
 # Latinate plurals so I put this in. It's not wrong; shut up!
     if ((substr($message, $l-1, $l) == "ii") || $message[$l] == "a")
-        return(true);
+        return true;
 
-        return(false);
+    return false;
 }
 
 function pluralize($message)
 {   if (!isPlural($message))
     { $l = strlen($message) - 1;
         if (substr($message, $l-1, $l) == "um")
-            return(substr($message, 0, $l-1)."a");
+            return substr($message, 0, $l-1)."a";
         if (substr($message, $l, $l) == "y")
-            return(substr($message, 1, $l-1)."ies");
-        return($message."s"); # Is there a rule for "s" vs "es"?
+            return substr($message, 1, $l-1)."ies";
+        return $message."s" ; # Is there a rule for "s" vs "es"? Yes, there is!
     }
     else
-    return($message);
+    return $message;
+}
+
+function pastify($message)
+{   $l = strlen($message);
+    if ($message[$l-1] == "p" && ($l > 2 && isVowel($message[$l-1]) && !isVowel($message[$l-2])))
+        return $message."ped";
+    if ($message[$l-1] == "n") return $message."ned"; # Yes, I'm aware
+    if ($message[$l-1] == "e") return $message."d";
+    else return $message."ed";
 }
 
 function hasWord($anArray, $aWord)
@@ -63,15 +74,30 @@ function hasWord($anArray, $aWord)
     while ($z < $high)
     {   if (  strToLower($anArray[$z]) == strToLower($aWord)
            || pluralize(strToLower($anArray[$z])) == strToLower($aWord)
+           || pastify(strToLower($anArray[$z])) == strToLower($aWord)
            )
-            return(true);
+            return true;
         $z++;
     }
-    return(false);
+    return false;
+}
+
+function verbing($message)
+{   $l = strlen($message);
+    if ($message[$l-1] == "p" && ($l > 2 && isVowel($message[$l-1]) && !isVowel($message[$l-2])))
+        return $message."ping";
+    if ($message[$l-1] == "d" && ($l > 2 && isVowel($message[$l-1]) && !isVowel($message[$l-2])))
+        return $message."ding";
+    return $message."ing";
+}
+
+function isVerbing($message)
+{   $l = strlen($message);
+    return strtolower(substr($message, $l-3, $l-1)) == "ing";
 }
 
 function isPossesive($aWord)
-{   return(strlen($aWord > 2)) && ($aWord[strlen($aWord)-1] == "'" || $aWord[strlen($aWord)-2] == "'");
+{   return strlen($aWord) > 2 && ($aWord[strlen($aWord)-1] == "'" || $aWord[strlen($aWord)-2] == "'");
 }
 
 function pickoutKeyword($message)
@@ -89,9 +115,9 @@ function pickoutKeyword($message)
 
     }
     if (count($newKeywords) > 0)
-	return($newKeywords[rand(0, count($newKeywords)-1)]);
+	return $newKeywords[rand(0, count($newKeywords)-1)];
     else
-	return("derpa");
+	return null;
 
 }
 
